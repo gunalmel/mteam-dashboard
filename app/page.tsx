0bfb6445a7342e7acb27cbc2ec0d51f2bf8ae652
Melih Gunal <gunalmel@med.umich.edu>
2024-06-27 18:12:00 -0400

    refs #4 - Added transitions in the same way the demo application at https://github.com/thedevagyasharma/mteam-dashboard. However, the first 2 transitions overlap suggesting an error in deciding the borders of transitions

--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,257 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
-import styles from '@/app/ui/home.module.css';
-import { lusitana } from '@/app/ui/fonts';
-import Image from 'next/image';
+'use client';
+import React, {useEffect, useState} from 'react';
+// see https://github.com/plotly/react-plotly.js/issues/272 for why we are using dynamic import for react-plotly to be built by nextjs
+import dynamic from 'next/dynamic';
+import Papa from 'papaparse';
+import VideoPlayer from '@/app/ui/dashboard/VideoPlayer';
+import {Annotations, Data, Layout, Shape} from 'plotly.js';
 
-export default function Page() {
-  return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        <AcmeLogo />
-      </div>
-        <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-            <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-                <div className={styles.shape}>Test the shape using css module</div>
-                <div
-                    className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
-                />
-                <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-                    <strong>Welcome to Acme.</strong> This is the example for the{' '}
-                    <a href="https://nextjs.org/learn/" className="text-blue-500">
-                        Next.js Learn Course
-                    </a>
-                    , brought to you by Vercel.
-                </p>
-                <Link
-                    href="/login"
-                    className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-                >
-                    <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6"/>
-                </Link>
+const Plot = dynamic(() => import('react-plotly.js'), { ssr: false })
+
+function timeToSeconds(timeStr: string) {
+    const parts = timeStr.split(':');
+    return parseInt(parts[0]) * 3600 + parseInt(parts[1]) * 60 + parseInt(parts[2]);
+}
+
+const icons = {
+    "Pulse Check": "💓",
+    "Select Epinephrine": "💉",
+    "Select Amiodarone": "💉",
+    "Shock": "⚡",
+    "Order Cooling": "🌡️",
+    "Order EKG": "📈",
+    "Intubation": "💨",
+    "Order X-Ray": "☢️",
+    "Labs": "🧪",
+    "Lung Sounds": "🩺"
+};
+
+function getIcon(subAction: string){
+    const iconNames = Object.keys(icons) as Array<keyof typeof icons>;
+    const iconName = iconNames[iconNames.findIndex(k => subAction.includes(k))];
+    return icons[iconName] || '';
+}
+
+const Explanation = () => (
+    <div className={'pl-28'}>
+        <div className="mb-2">
+            <hr className="border-t-2 border-green-600 w-6 inline-block my-1"/>
+            : Compression Interval
+        </div>
+        <div className="grid grid-cols-3 gap-2">
+            <div className="flex items-center">
+                <span className="mr-1">💓</span> Pulse Check
             </div>
-            <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-                <Image
-                    src="/hero-desktop.png"
-                    width={1000}
-                    height={760}
-                    className="hidden md:block"
-                    alt="Screenshots of the dashboard project showing desktop version"
-                />
-                <Image
-                    src="/hero-mobile.png"
-                    width={500}
-                    height={620}
-                    className="block md:hidden"
-                    alt="Screenshots of the dashboard project showing mobile version"
+            <div className="flex items-center">
+                <span className="mr-1">🩺</span> Lung Sounds
+            </div>
+            <div className="flex items-center">
+                <span className="mr-1">☢️</span> X-Ray
+            </div>
+            <div className="flex items-center">
+                <span className="mr-1">💉</span> Epinephrine or Amiodarone
+            </div>
+            <div className="flex items-center">
+                <span className="mr-1">📈</span> EKG
+            </div>
+            <div className="flex items-center">
+                <span className="mr-1">🌡️</span> Cool
+            </div>
+            <div className="flex items-center">
+                <span className="mr-1">⚡</span> Shock
+            </div>
+            <div className="flex items-center">
+                <span className="mr-1">💨</span> Intubate
+            </div>
+            <div className="flex items-center">
+                <span className="mr-1">🧪</span> New Labs
+            </div>
+        </div>
+    </div>
+);
+
+const phaseColors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf'];
+
+const cprStartSubActionNames = ['Begin CPR', 'Enter CPR'];
+const cprStopSubActionNames = 'Stop CPR';
+const csvColHeaders ={
+    action: 'Action/Vital Name',
+    subAction: 'SubAction Name',
+    timestamp: 'Time Stamp[Hr:Min:Sec]'
+};
+const subActionExclusions = ['Begin CPR', 'Enter CPR', 'Stop CPR'];
+function shouldPlotAction(action: string, subAction: string){
+    return action && subAction && !subActionExclusions.includes(subAction) && action.includes('(action)') && !action.includes('User Introduction');
+}
+function doesCPRStart(subAction: string){
+    return cprStartSubActionNames.includes(subAction);
+}
+function doesCPRStop(subAction: string){
+    return cprStopSubActionNames === subAction;
+}
+function isTransitionBoundary(action: string){
+    return action && action.includes('(action)');
+}
+
+function createActionsScatterData(timeInSeconds: Array<number>, iconText: Array<string>, annotations: Array<string> ) : Partial<Data>{
+    return {
+        x: timeInSeconds,
+        y: new Array(timeInSeconds.length).fill(1),
+        mode: 'text',
+        type: 'scatter',
+        text: iconText,
+        hovertext: annotations,
+        hoverinfo: 'text',
+        textposition: 'top center',
+        marker: {size: 12}
+    };
+}
+
+function createCompressionLine(seconds: Array<number>, hoverText: Array<string>) : Partial<Data> {
+    return {
+        x: seconds,
+        y: [0.98, 0.98],
+        mode: 'lines',
+        type: 'scatter',
+        text: '',
+        hovertext: hoverText,
+        hoverinfo: 'text',
+        textposition: 'top center',
+        marker: {size: 12},
+        line: {
+            color: 'rgb(0, 150, 0)'
+        }
+    };
+}
+
+function createTransition(phaseName: string, start: number, end: number, fillColor: string): Partial<Shape> {
+    return {
+        type: 'rect',
+        xref: 'x',
+        yref: 'paper',
+        x0: start,
+        x1: end,
+        y0: 0,
+        y1: 1,
+        fillcolor: fillColor,
+        line: { width: 0 },
+        layer: 'below',
+            name: phaseName
+    }
+}
+
+function createTransitionAnnotation(text: string, start: number, fontColor: string) : Partial<Annotations> {
+    return {
+        xref: 'x', //'paper',
+        yref: 'paper',
+        x: start+2,
+        y: 0.8,
+        xanchor: 'center',
+        yanchor: 'middle',
+        text: text.replace('(action)', ''),
+        showarrow: false,
+        textangle: '270',
+        font: {
+            size: 9,
+            color: fontColor,
+        },
+    };
+}
+
+const Page = () => {
+    const [data, setData] = useState<Array<Partial<Data>>>([]);
+    const [layout, setLayout] = useState({});
+
+    useEffect(() => {
+        const phaseMap:{[key:string]:{start: number, end:number}} = {};
+        const timestampsInSeconds: number[] = [];
+        const actions = [];
+        const actionIcons : Array<string> = [];
+        let compressionLine: {seconds: Array<number>, hoverText: Array<string>} ={
+            seconds:[],
+            hoverText: []
+        };
+        const actionAnnotations: string[] = [];
+        const compressionLines: Array<Partial<Data>> = [];
+
+        //TODO: 06-28-2024 - Mel Gunal: We'd better make a hook to use Papa.parse to set the state of the graph.
+        Papa.parse('https://raw.githubusercontent.com/thedevagyasharma/mteam-dashboard/main/src/Data_sample2/timeline-multiplayer%20(32).csv', {
+            download: true,
+            header: true,
+            step: function (row : Papa.ParseStepResult<{[key: string]: string}> ) {
+                const {[csvColHeaders.action]:action,
+                       [csvColHeaders.subAction]:subAction,
+                       [csvColHeaders.timestamp]:timestamp} = row.data;
+                const timestampInSeconds = timeToSeconds(timestamp);
+
+                if (doesCPRStart(subAction)) {
+                    compressionLine.seconds.push(timestampInSeconds);
+                    compressionLine.hoverText.push(timestamp);
+                }
+                else if (doesCPRStop(subAction)) {
+                    compressionLine.seconds.push(timestampInSeconds);
+                    compressionLine.hoverText.push(timestamp);
+                    compressionLines.push(createCompressionLine(compressionLine.seconds, compressionLine.hoverText));
+                    compressionLine.seconds=[];
+                    compressionLine.hoverText=[];
+                }
+
+                if(isTransitionBoundary(action)){
+                    if(!phaseMap[action]){
+                        phaseMap[action] = {start: timestampInSeconds, end: timestampInSeconds};
+                    } else {
+                        phaseMap[action].end = timestampInSeconds;
+                    }
+                }
+
+                if (shouldPlotAction(action, subAction)) {
+                    actions.push(action);
+                    actionAnnotations.push(`${timestamp}, ${subAction}`);
+                    actionIcons.push(getIcon(subAction));
+                    timestampsInSeconds.push(timestampInSeconds);
+                }
+            },
+            complete: function () {
+                const actionsScatterData = createActionsScatterData(timestampsInSeconds, actionIcons, actionAnnotations);
+
+                const {transitionShapes,transitionAnnotations} = Object.keys(phaseMap).reduce<{transitionShapes:Array<Partial<Shape>>,transitionAnnotations:Array<Partial<Annotations>>}>((accumulator, action, index) => {
+                    accumulator.transitionShapes.push(createTransition(action, phaseMap[action].start, phaseMap[action].end, phaseColors[index % phaseColors.length] + '33'));
+                    accumulator.transitionAnnotations.push(createTransitionAnnotation(action, phaseMap[action].start, phaseColors[index % phaseColors.length]));
+                    return accumulator;
+                },{transitionShapes:[], transitionAnnotations:[]});
+
+                const layoutConfig: Partial<Layout> = {
+                    title: 'Clinical Review Timeline',
+                    xaxis: {title: 'Time (seconds)', showgrid: false},
+                    yaxis: {visible: false, range: [0, 2]},
+                    showlegend: false,
+                    shapes: transitionShapes,
+                    annotations: transitionAnnotations,
+                    autosize: true
+                };
+
+                setData([actionsScatterData, ...compressionLines]);
+                setLayout(layoutConfig);
+            }
+        });
+
+    }, []);
+
+    return (
+        <div className="flex flex-col justify-evenly">
+            <VideoPlayer/>
+            <Explanation/>
+            <div className="bg-white p-4" style={{width: '100%', height: '600px'}}>
+                <Plot
+                    data={data}
+                    layout={layout}
+                    style={{width: '100%', height: '100%'}}
+                    useResizeHandler={true}// Ensure the plot adjusts size when container changes
                 />
             </div>
         </div>
-    </main>
-  );
-}
+    );
+};
+
+export default Page;
+
--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,24 @@
 /** @type {import('next').NextConfig} */
 
+import TerserPlugin  from 'terser-webpack-plugin';
 const nextConfig = {
   experimental: {
     ppr: 'incremental',
+  },
+  // productionBrowserSourceMaps: true,
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      config.optimization.minimizer = [
+        new TerserPlugin({
+          terserOptions: {
+            mangle: false,
+            keep_classnames: true,
+            keep_fnames: true,
+          },
+        }),
+      ];
+    }
+    return config;
   },
 };
 
